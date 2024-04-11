dcec883c708856f95356c7db960b14257a9ce4d4
bebex <joel.parrenooo@gmail.com>
2024-04-11 20:37:53 +0800

    initial commit

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,49 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create') }}
+        </h2>
+    </x-slot>
+    <div class="py-3">
+	    <div class="sm:px-12 flex justify-center">
+	    <div class="p-4 sm:p-9 bg-white shadow sm:rounded-lg w-1/2">
+	        <div class="max">
+	        	<div>
+				@if($errors->any())
+					<ul>
+						@foreach($errors->all() as $error)
+						<li class="peer-invalid:visible text-red-700 font-light">{{$error}}</li>
+						@endforeach
+					</ul>
+				@endif
+				</div>
+	        	<form method="POST" action="{{route('product.store')}}">
+					@csrf
+					@method('post')
+		            <div>
+		                <x-input-label for="product" :value="__('Product Name')" />
+		                <x-text-input id="name" class="block mt-1 w-full" name="name" required autofocus />
+		            </div>
+		            <div>
+		                <x-input-label for="qty" :value="__('Quantity')" />
+		                <x-text-input id="qty" class="block mt-1 w-full" name="qty" required />
+		            </div>
+		            <div>
+		                <x-input-label for="price" :value="__('Price')" />
+		                <x-text-input id="price" class="block mt-1 w-full" name="price" required />
+		            </div>
+		            <div>
+		                <x-input-label for="description" :value="__('Description')" />
+		                <x-text-input id="description" class="block mt-1 w-full" name="description" required />
+		            </div>
+		            <div>
+		                <x-primary-button class="mt-4">
+			                {{ __('Add') }}
+			            </x-primary-button>
+		            </div>
+	        	</form>
+	        </div>
+	    </div>
+	</div>
+    </div>
+</x-app-layout>
